b5c727507fd1829cb92bd76331616c0d6b5a27dc
stepan881 <stepanstepanov180@gmail.com>
2021-01-29 15:58:58 +0300

    add mobile

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,40 @@ window.addEventListener('scroll', function() {
   headerColor();
 });
 
+let windowWidth = window.innerWidth;
+window.addEventListener(`resize`, () => {
+  windowWidth = window.innerWidth;
+  console.log('windowWidth: ', windowWidth);
+})
+
 function headerColor() {
   const header = document.querySelector(`.header`);
   const headerTexr = document.querySelector(`h1`).getBoundingClientRect().top + pageYOffset;
 
-  if (scrollTop <= (headerTexr-50)) {
-    header.classList.add(`header--active`)
+  if (windowWidth >= 769 ) {
+    if (scrollTop <= (headerTexr-50)) {
+      header.classList.add(`header--active`)
+    } else {
+      header.classList.remove(`header--active`)
+    }
   } else {
     header.classList.remove(`header--active`)
   }
+
+
 }
 headerColor();
+
+function openMenu() {
+
+  const btn = document.querySelector(`.header__btn`);
+  const menu = document.querySelector(`.nav`);
+  btn.addEventListener(`click`, evt => {
+    evt.preventDefault();
+
+    menu.classList.toggle(`nav--close`)
+    btn.classList.toggle(`header__btn--open`)
+  })
+
+};
+openMenu();
